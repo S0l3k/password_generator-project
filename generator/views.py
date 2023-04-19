ae2321c1ec85f6b96f2a00e5b9e4f415060c6cad
S0l3k <turtleteamwcg@gmail.com>
2023-04-19 20:43:06 +0300

    add: page "about" fix: style with bootstrap

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -7,4 +8,23 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghjklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHJKLMOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('~`!@#$%^&*()-_=+{}[]\|/;:''""<>,.?'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
